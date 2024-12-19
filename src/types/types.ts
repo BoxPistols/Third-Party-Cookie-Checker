2ae1db35f715a8e15dc37cf71d49c054f3d43742
a.ito <10333049+BoxPistols@users.noreply.github.com>
2024-12-19 10:21:50 +0900

    type

--- src/types/types.ts
+++ src/types/types.ts
@@ -74,3 +74,10 @@ export interface ResourceInfo {
   isThirdParty: boolean;
   category: unknown;
 }
+
+// CookieDetail
+export interface CookieDetail {
+  domain: string;
+  purpose: string;
+  description: string;
+}
